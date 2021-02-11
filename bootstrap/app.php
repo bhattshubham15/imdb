0cfb2269830cc5cf7f3778cd670f172b23b94dce
shubham.bhatt <shubham.bhatt@kelltontech.com>
2021-02-11 17:28:33 +0530

    cors handle2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 (new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
     dirname(__DIR__)
@@ -17,7 +17,7 @@ date_default_timezone_set(env('APP_TIMEZONE', 'UTC'));
 | that serves as the central piece of this framework. We'll use this
 | application as an "IoC" container and router for this framework.
 |
-*/
+ */
 
 $app = new Laravel\Lumen\Application(
     dirname(__DIR__)
@@ -36,7 +36,7 @@ $app->withEloquent();
 | register the exception handler and the console kernel. You may add
 | your own bindings here if you like or you can make another file.
 |
-*/
+ */
 
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
@@ -57,7 +57,7 @@ $app->singleton(
 | your configuration directory it will be loaded; otherwise, we'll load
 | the default version. You may register other files below as needed.
 |
-*/
+ */
 
 $app->configure('app');
 $app->configure('cors');
@@ -71,7 +71,7 @@ $app->configure('cors');
 | be global middleware that run before and after each request into a
 | route or middleware that'll be assigned to some specific routes.
 |
-*/
+ */
 
 // $app->middleware([
 //     App\Http\Middleware\ExampleMiddleware::class
@@ -82,7 +82,7 @@ $app->middleware([
 ]);
 
 $app->routeMiddleware([
-    Fruitcake\Cors\HandleCors::class,
+    \App\Http\Middleware\Cors::class, //cors added here
     'auth' => App\Http\Middleware\Authenticate::class,
 ]);
 /*
@@ -94,7 +94,7 @@ $app->routeMiddleware([
 | are used to bind services into the container. Service providers are
 | totally optional, so you are not required to uncomment this line.
 |
-*/
+ */
 
 // $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
@@ -111,12 +111,12 @@ $app->register(Fruitcake\Cors\CorsServiceProvider::class);
 | the application. This will provide all of the URLs the application
 | can respond to, as well as the controllers that may handle them.
 |
-*/
+ */
 
 $app->router->group([
     'namespace' => 'App\Http\Controllers',
 ], function ($router) {
-    require __DIR__.'/../routes/web.php';
+    require __DIR__ . '/../routes/web.php';
 });
 
 return $app;
